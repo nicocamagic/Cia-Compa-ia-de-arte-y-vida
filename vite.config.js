f3a5a228b32b3682a37272e0d4d609cbbe7a99bc
NICOLAS CAMARGO LIZARAZO <98283511+nicocamagic@users.noreply.github.com>
2025-08-21 00:38:47 +0000

    cambios

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
-import { plugin } from 'postcss'
 
 export default defineConfig({
     base: "/Cia-Compa-ia-de-arte-y-vida",
